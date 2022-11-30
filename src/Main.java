3a03451222ee51472f2ec7616594921257bba36f
unknown <sazidahmednassir@gmail.com>
2022-11-29 22:16:42 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My First Code");
+        System.out.println("Product id : 101.");
+        System.out.println("Product title : iphone15.");
+        System.out.println("Product price : 1895 euros.");
+        System.out.println("Product Description : perfect product with best image quality.");
+        System.out.println("Product category: Phone.\nMy First Name: Sazid Ahmed Nassir.");
+
+    }
+}
