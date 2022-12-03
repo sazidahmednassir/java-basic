7132e046d8b54ed231460f46f19dc6a5477f1105
unknown <sazidahmednassir@gmail.com>
2022-12-02 20:37:34 -0800

    addinng new task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // first task
         System.out.println("My First Code");
         System.out.println("Product id : 101.");
         System.out.println("Product title : iphone15.");
@@ -8,6 +9,19 @@ public class Main {
         System.out.println("Product category: Phone.\nMy First Name: Sazid Ahmed Nassir.");
         System.out.println("3 \t 4");
         System.out.println(" \"Java Programming\" ");
+
+        // second task
+        int id = 101;
+        String title = "iphone15";
+        String price = "1895 euros";
+        String description = "perfect product with best image quality";
+        String category = "phone";
+
+        System.out.println("id = "+id);
+        System.out.println("title = "+title);
+        System.out.println("price = "+price);
+        System.out.println("description = "+description);
+        System.out.println("category = "+category);
 
     }
 }
